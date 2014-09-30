fc06032ca2b0f8736910ed91a85a116ff655bace
Rodrigo Casagrande de Oliveira <rodrigo.casagrande.oliveira@gmail.com>
2014-09-30 17:16:10 -0300

    Created Rails application and added status update

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+UpMundo::Application.config.secret_token = '1d9986cf261186cbab5a200ead617b4960dfec2ef8672841f43e666af98f8f4972f830cb131868600abe80cebeaf7e80a7b26545566d5bc03992c5ab0c2d3624'
